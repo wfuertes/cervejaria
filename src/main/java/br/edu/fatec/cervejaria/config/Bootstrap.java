d55a2f8e9e4c59ac2234541c3c6070d1ba100c2e
Willian Batista <wbatista@kenzanmedia.com>
2015-06-09 09:39:13 -0300

    Adicionado JacksonJaxbJsonProvider para PascalCase e insensitive case;

--- src/main/java/br/edu/fatec/cervejaria/config/Bootstrap.java
+++ src/main/java/br/edu/fatec/cervejaria/config/Bootstrap.java
@@ -1,9 +1,14 @@
 package br.edu.fatec.cervejaria.config;
 
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.Scopes;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import com.google.inject.persist.PersistFilter;
 import com.google.inject.persist.jpa.JpaPersistModule;
 import com.google.inject.servlet.GuiceServletContextListener;
@@ -33,14 +38,31 @@ public class Bootstrap extends GuiceServletContextListener {
 
                 install(new CervejariaModule());
                 install(new JpaPersistModule(CERVEJARIA_PERSISTENCE_UNIT));
-                
-                bind(JacksonJaxbJsonProvider.class).in(Scopes.SINGLETON);
 
                 final Map<String, String> params = new HashMap<>();
                 params.put(PackagesResourceConfig.PROPERTY_PACKAGES, "br.edu.fatec");
 
                 filter("/*").through(PersistFilter.class);
                 serve("/api/*").with(GuiceContainer.class, params);
+            }
+
+            @Provides
+            @Singleton
+            public ObjectMapper getObjectMapper() {
+
+                final ObjectMapper mapper = new ObjectMapper();
+                mapper.registerModule(new JaxbAnnotationModule());
+                mapper.setPropertyNamingStrategy(PropertyNamingStrategy.PASCAL_CASE_TO_CAMEL_CASE);
+                mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+
+                return mapper;
+            }
+
+            @Provides
+            @Singleton
+            public JacksonJaxbJsonProvider getJacksonJaxbJsonProvider(ObjectMapper mapper) {
+
+                return new JacksonJaxbJsonProvider(mapper, JacksonJaxbJsonProvider.DEFAULT_ANNOTATIONS);
             }
         });
     }
